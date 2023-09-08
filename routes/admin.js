7a9b3382b80b00afb883ae85a1642bb9ebb0655f
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-09-08 10:15:29 +0200

    Start working with templating enginees, and install ejs, pug and handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const routeDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GE
 
 router.get("/add-product", (req, res, next) => {
@@ -14,8 +16,11 @@ router.get("/add-product", (req, res, next) => {
 // /admin/add-product => POST
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+// module.exports = router;
+
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,12 @@ const express = require("express");
 
 const routeDir = require("../util/path");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(routeDir, "views", "shop.html"));
 });
 
