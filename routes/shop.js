1a81b1168ce65129718860e5948808f52f05b276
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-09-13 14:09:58 +0200

    Update code, add controllers

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,11 @@
 const path = require("path");
+
 const express = require("express");
 
-const routeDir = require("../util/path");
-
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  // console.log(adminData.products);
-  // res.sendFile(path.join(routeDir, "views", "shop.html"));
-  res.render("shop");
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
